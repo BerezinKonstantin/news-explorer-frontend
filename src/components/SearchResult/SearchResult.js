f35806340c683fdf6e1361b4c7da7a09dc4b7390
Berezin Konstantin <berezinko90@gmail.com>
2020-11-10 21:18:08 +0700

    preloader fix

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -19,55 +19,55 @@ function SearchResult(props) {
       (isSearchSuccess || props.isRenderLoading || isNotResult) &&
       (
         <div className="search-result">
-        { isSearchSuccess && 
-          (
+          { props.isRenderLoading && 
             <>
-              <h1 className="search-result__title">
-                Результаты поиска
-              </h1>
-              <NewsCardList
-                cards={cards}
-                keyword={props.keyword}
-                onSaveArticle={props.onSaveArticle}
-                onDeleteArticle={props.onDeleteArticle}
-              />
-              { (cards.length < props.searchResult.length ) &&
-                (<button
-                className="search-result__more_button"
-                type="button"
-                onClick={showMoreCards}
-                >
-                  Показать ещё
-                </button>)
-              } 
-            </>
-          )
-        } 
-        { props.isRenderLoading && 
-          <>
-            <Preloader />
-            <p className="search-result__subtitle_not-result">
-                Идет поиск новостей...
-            </p>
-          </>
-        }
-        { isNotResult &&
-          (
-            <>
-              <img
-                 className="search-result__img_not-result"
-                 src={notFoundIcon}
-                 alt='Ничего не найдено'
-              />
-              <h2 className="search-result__title_not-result">
-                Ничего не найдено
-              </h2>
-              <p className="search-result__subtitle_not-result">
-                К сожалению, по вашему запросу ничего не найдено.
+              <Preloader />
+              <p className="search-result__subtitle">
+                  Идет поиск новостей...
               </p>
             </>
-          )
-        }
+          }
+          { isSearchSuccess && 
+            (
+              <>
+                <h1 className="search-result__title">
+                  Результаты поиска
+                </h1>
+                <NewsCardList
+                  cards={cards}
+                  keyword={props.keyword}
+                  onSaveArticle={props.onSaveArticle}
+                  onDeleteArticle={props.onDeleteArticle}
+                />
+                { (cards.length < props.searchResult.length ) &&
+                  (<button
+                  className="search-result__more_button"
+                  type="button"
+                  onClick={showMoreCards}
+                  >
+                    Показать ещё
+                 </button>)
+                } 
+              </>
+            )
+          } 
+          { isNotResult &&
+            (
+              <>
+                <img
+                   className="search-result__img_not-result"
+                   src={notFoundIcon}
+                   alt='Ничего не найдено'
+                />
+                <h2 className="search-result__title_not-result">
+                  Ничего не найдено
+                </h2>
+                <p className="search-result__subtitle">
+                  К сожалению, по вашему запросу ничего не найдено.
+                </p>
+              </>
+            )
+          }
         </div>
       )
     }
