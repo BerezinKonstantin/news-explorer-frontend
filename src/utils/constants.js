bee89015d52ff327ace3a1d57d64760dad4614e5
KonstantinBerezin <berezinko90@gmail.com>
2022-11-14 20:31:48 +0500

    config changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const basedUrlNewsApi = "https://nomoreparties.co/news/v2/everything?";
-export const basedUrlMainApi = "http://localhost:3001";
+export const basedUrlMainApi = "https://news-explorer-api.onrender.com";
 export const apiKey = "68f1c98bd386460cb603e368a24a99b3";
