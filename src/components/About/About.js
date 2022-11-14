2e52cf470a470e6bf4d1aa5215a94df09a055410
KonstantinBerezin <berezinko90@gmail.com>
2022-11-14 13:09:45 +0500

    Добавлены ссылки на гитхаб

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import avatar from "../../images/avatar.jpg";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function About() {
+  const currentUser = useContext(CurrentUserContext);
   return (
     <div className="about">
       <img className="about__avatar" alt="Аватар пользователя" src={avatar} />
       <div className="about__wrapper">
         <h2 className="about__title">Об авторе</h2>
         <p className="about__text">
-          Привет, меня зовут Константин!
+          Привет, {currentUser.name ? currentUser.name + "," : ""} меня зовут
+          Константин!
           <br />
           Это мой дипломный проект по завершению курса Frontend разработчика.
           Это сервис - агрегатор новостей.
@@ -19,6 +22,27 @@ function About() {
           что - вперед, попробуйте!
           <br />
           Рад, что вы зашли)
+        </p>
+        <p className="about__github">
+          Код проекта на GitHub:
+          <div className="about__github-link-wrap">
+            <a
+              className="about__github-link"
+              href="https://github.com/BerezinKonstantin/news-explorer-frontend"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Frontend
+            </a>
+            <a
+              className="about__github-link"
+              href="https://github.com/BerezinKonstantin/news-explorer-api"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Backend
+            </a>
+          </div>
         </p>
       </div>
     </div>
