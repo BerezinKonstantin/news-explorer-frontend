73a55cc2406d9b03dd260bb8ebee185bcf96589e
KonstantinBerezin <berezinko90@gmail.com>
2022-11-14 12:31:55 +0500

    Исправлены падежи

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -4,14 +4,19 @@ import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 function SavedNewsHeader(props) {
   const articles = props.savedArticles;
   const numberOfArticles = articles.length;
-
   const currentUser = useContext(CurrentUserContext);
 
   return (
     <div className="saved-news-header">
       <p className="saved-news-header__sub-title">Сохранённые статьи</p>
       <h1 className="saved-news-header__title">
-        {currentUser.name}, у вас {numberOfArticles} сохранённых статей
+        {currentUser.name}, у вас {numberOfArticles}
+        {numberOfArticles === 1 ? " сохранённая" : " сохранённых"}
+        {numberOfArticles === 1
+          ? " статья"
+          : numberOfArticles <= 4
+          ? " статьи"
+          : " статей"}
       </h1>
       {props.sortedKeywords.length > 0 && (
         <p className="saved-news-header__info">
@@ -25,9 +30,10 @@ function SavedNewsHeader(props) {
             <>
               и
               <span className="saved-news-header__info-bold">
-                &nbsp;{props.sortedKeywords.length - 2}-м&nbsp;
+                &nbsp;{props.sortedKeywords.length - 2}-
+                {props.sortedKeywords.length === 3 ? "му" : "м"}&nbsp;
               </span>
-              другим
+              {props.sortedKeywords.length === 3 ? "другому" : "другим"}
             </>
           )}
         </p>
